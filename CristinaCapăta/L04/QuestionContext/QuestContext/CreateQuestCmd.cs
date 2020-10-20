7348dc33278ae70399b299bf3bf038d968dc1869
cristina-capata <54958926+cristina-capata@users.noreply.github.com>
2020-10-20 23:28:08 +0300

    L04

---
+++ CristinaCapăta/L04/QuestionContext/QuestContext/CreateQuestCmd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace QuestionContext.QuestContext
+{
+    public struct CreateQuestCmd
+    {
+        [Required]
+        public string Titlu {get; private set;}
+        public string Descriere {get; private set;}
+        public string Tag{get; private set;}
+
+        public CreateQuestCmd(string Titlu, string Descriere, string Tag)
+        {
+            this.Titlu=Titlu;
+            this.Descriere=Descriere;
+            this.Tag=Tag;
+        }
+    }
+}
---
+++ CristinaCapăta/L04/QuestionContext/QuestContext/CreateQuestResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharp.Choices;
+
+namespace QuestionContext.QuestContext
+{
+    [AsChoice]
+    public static partial class CreateQuestResult
+    {
+        public interface ICreateQuestResult { }
+
+        public class QuestCreated: ICreateQuestResult
+        {
+           public string Titlu {get; private set;}
+            public string Descriere {get; private set;}
+            public string Tag {get; private set;}
+
+            public QuestCreated(string Titlu, string Descriere, string Tag)
+            {
+                this.Titlu=Titlu;
+                this.Descriere=Descriere;
+                this.Tag=Tag;
+            }
+        }
+
+        public class QuestNotCreated: ICreateQuestResult
+        {
+            public IEnumerable<string> ValidationError {get; private set;}
+            public QuestionValidationFailed(IEnumerable<string> error)
+            {
+                ValidationError=error.AsEnumerable();
+            }
+        }
+
+    }
+}
