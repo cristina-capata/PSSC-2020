f81acae03927e9e06b87f8e0da471dddb64ff54c
cristina-capata <54958926+cristina-capata@users.noreply.github.com>
2020-11-05 23:49:38 +0200

    L06

---
+++ CristinaCapăta/L06/Reply.Schema/Inputs/AckToQuestionOwnerCmd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reply.Domain.Inputs
+{
+    public class AckToQuestionOwnerCmd
+    {
+        public int QuestionId { get; private set; }
+        public int ReplyId { get; private set; }
+        public string ReplyBody { get; private set; }
+
+        public AckToQuestionOwnerCmd(int questionId, int replyId, string replyBody)
+        {
+            this.QuestionId = questionId;
+            this.ReplyId = replyId;
+            this.ReplyBody = replyBody;
+        }
+    }
+}
---
+++ CristinaCapăta/L06/Reply.Schema/Inputs/CreateReplyCmd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reply.Domain.Inputs
+{
+    public class CreateReplyCmd
+    {
+        public int QuestionId { get; }
+        public int AuthorId { get; }
+        public string ReplyBody { get; }
+
+        public CreateReplyCmd (int questionId, int authorId, string replyBody)
+        {
+            this.QuestionId = questionId;
+            this.AuthorId = authorId;
+            this.ReplyBody = replyBody;
+        }
+    }
+}
